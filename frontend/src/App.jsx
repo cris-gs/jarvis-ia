6720ec4811b58130882498a7ea8cd7fc2e4b74b7
Derian Rodriguez <dmrodriguez2000@gmail.com>
2023-05-04 22:57:18 -0600

    Introduction only once

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [jarvisResponse, setJarvisResponse] = useState(null);
   const [jarvisActive, setJarvisActive] = useState(false);
   const [facialRecognitionActive, setFacialRecognitionActive] = useState(false);
+  const [intro, setIntro] = useState(true);
 
   const jarvisSay = (message) => {
     return new Promise((resolve, reject) => {
@@ -24,18 +25,21 @@ const App = () => {
     setIsChecked(!isChecked);
     if (!isChecked) {
       setJarvisActive(true);
-      await jarvisSay("Hola, soy Jarvis TEC su asistente personal. ¿Qué modelo quiere usar?");
-      await jarvisSay("Opciones disponibles");
-      await jarvisSay("1 si quiere predecir el precio del bitcoin.");
-      await jarvisSay("2 si quiere predecir el precio de un automóvil.");
-      await jarvisSay("3 si quiere que le recomiende películas.");
-      await jarvisSay("4 si quiere clasificar la calidad de un vino.");
-      await jarvisSay("5 si quiere predecir la cantidad de inventario de una compañía.");
-      await jarvisSay("6 si quiere predecir la tarifa de los viajes de taxi.");
-      await jarvisSay("7 si quiere clasificar si un cliente se va a pasar de compañía celular.");
-      await jarvisSay("8 si quiere predecir el delay de los viajes de avión.");
-      await jarvisSay("9 si quiere predecir la masa corporal de un paciente.");
-      await jarvisSay("10 si quiere predecir el precio de un aguacate.");
+      if (intro) {
+        await jarvisSay("Hola, soy Jarvis TEC su asistente personal. ¿Qué modelo quiere usar?");
+        await jarvisSay("Opciones disponibles");
+        await jarvisSay("1 si quiere predecir el precio del bitcoin.");
+        await jarvisSay("2 si quiere predecir el precio de un automóvil.");
+        await jarvisSay("3 si quiere que le recomiende películas.");
+        await jarvisSay("4 si quiere clasificar la calidad de un vino.");
+        await jarvisSay("5 si quiere predecir la cantidad de inventario de una compañía.");
+        await jarvisSay("6 si quiere predecir la tarifa de los viajes de taxi.");
+        await jarvisSay("7 si quiere clasificar si un cliente se va a pasar de compañía celular.");
+        await jarvisSay("8 si quiere predecir el delay de los viajes de avión.");
+        await jarvisSay("9 si quiere predecir la masa corporal de un paciente.");
+        await jarvisSay("10 si quiere predecir el precio de un aguacate.");
+        setIntro(false);
+      }
       await jarvisSay("Por favor, indique la opción que desea seleccionar diciendo la palabra 'opción' seguida del número correspondiente.");
       // await jarvisSay("La dejo con mi colega, ten paciencia porque es media tontita.")
       await jarvisSay("Tomando indicaciones...");
